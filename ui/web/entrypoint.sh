cbdfd2a1b979bab99702f68cd6d3fd984841e26b
Константин <menace34@bk.ru>
2024-03-03 12:55:57 +0300

    fixed

--- ui/web/entrypoint.sh
+++ ui/web/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+# shellcheck disable=SC2164
+cd ui/web/
 #python manage.py makemigrations
-python manage.py migrate
+#python manage.py migrate
 celery -A web.celery:app worker -B -l info --detach
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
